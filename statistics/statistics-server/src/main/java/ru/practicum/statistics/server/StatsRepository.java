97f38517c805ac0b617fb3264ccca342f14a714d
Aleksey Melnikov <realxellor@gmail.com>
2024-06-26 14:17:41 +0300

    fix: more sql

--- statistics/statistics-server/src/main/java/ru/practicum/statistics/server/StatsRepository.java
+++ statistics/statistics-server/src/main/java/ru/practicum/statistics/server/StatsRepository.java
@@ -10,29 +10,31 @@ import java.util.List;
 
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
 
-    @Query(value = "SELECT distinct on (e.uri) e.app, e.uri , ee.hits from ( " +
+    @Query(value = "SELECT * FROM ( " +
+            "SELECT distinct on (e.uri) e.app, e.uri , ee.hits from ( " +
             " SELECT uri, count(uri) as hits " +
-            " FROM endpointhits WHERE timestamp >= ?1 and timestamp <= ?2 GROUP BY uri ORDER BY hits DESC ) ee " +
-            "JOIN endpointhits e on e.uri = ee.uri", nativeQuery = true)
+            " FROM endpointhits WHERE timestamp >= ?1 and timestamp <= ?2 GROUP BY uri ) ee " +
+            "JOIN endpointhits e on e.uri = ee.uri ) t ORDER BY t.hits DESC;", nativeQuery = true)
     List<IViewStatsDto> findEndpointHits(LocalDateTime start, LocalDateTime end);
 
-    @Query(value = "SELECT distinct on (ee.uri) e.app, ee.uri, ee.hits FROM ( " +
+    @Query(value = "SELECT * FROM ( " +
+            "SELECT distinct on (ee.uri) e.app, ee.uri, ee.hits FROM ( " +
             "SELECT uri, count(distinct ip) as hits from endpointhits WHERE timestamp >= ?1 and timestamp <= ?2 " +
             "group by uri ORDER BY hits DESC " +
-            ") ee JOIN endpointhits e on e.uri = ee.uri ",
-            nativeQuery = true)
+            ") ee JOIN endpointhits e on e.uri = ee.uri ) t ORDER BY t.hits DESC;", nativeQuery = true)
     List<IViewStatsDto> findEndpointHitsWithUniqueIp(LocalDateTime start, LocalDateTime end);
 
-    @Query(value = "SELECT distinct on (e.uri) e.app, e.uri , ee.hits from ( " +
+    @Query(value = "SELECT * FROM ( " +
+            "SELECT distinct on (e.uri) e.app, e.uri , ee.hits from ( " +
             " SELECT uri, count(uri) as hits " +
-            " FROM endpointhits WHERE uri in ?3 and timestamp >= ?1 and timestamp <= ?2 GROUP BY uri ORDER BY hits DESC ) ee " +
-            "JOIN endpointhits e on e.uri = ee.uri", nativeQuery = true)
+            " FROM endpointhits WHERE uri in ?3 and timestamp >= ?1 and timestamp <= ?2 GROUP BY uri ) ee " +
+            "JOIN endpointhits e on e.uri = ee.uri ) t ORDER BY t.hits DESC;", nativeQuery = true)
     List<IViewStatsDto> findEndpointHitsWithUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query(value = "SELECT distinct on (ee.uri) e.app, ee.uri, ee.hits FROM ( " +
+    @Query(value = "SELECT * FROM ( " +
+            "SELECT distinct on (ee.uri) e.app, ee.uri, ee.hits FROM ( " +
             "SELECT uri, count(distinct ip) as hits from endpointhits WHERE uri in ?3 and timestamp >= ?1 " +
             "and timestamp <= ?2 group by uri ORDER BY hits DESC " +
-            ") ee JOIN endpointhits e on e.uri = ee.uri ",
-            nativeQuery = true)
+            ") ee JOIN endpointhits e on e.uri = ee.uri ) t ORDER BY t.hits DESC;", nativeQuery = true)
     List<IViewStatsDto> findEndpointHitsWithUniqueIpWithUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
